e0dea9ac97ada26bac2655ce0278c913c369b2f3
Rameen <rameen@hyperext.com>
2025-05-13 16:00:23 +0100

    styling to weatherApp & code fixes

--- session-10/weatherApp/weatherApp.js
+++ session-10/weatherApp/weatherApp.js
@@ -6,10 +6,6 @@ Display the forecast to the user. Cater for an error when a user types a place t
 
 Add CSS to make the application more appealing and accessible for the user
 
-PSEUDOCODE
-
-
-// Async function to retrieve weather information
 
 //All DOM elements
 
@@ -21,17 +17,69 @@ function for output?*/
 
 //promise that is for waiting for users locationInput
 function locationInput() {
-    // let currency = 
+    // let location = 
     return new Promise((resolve, reject) => {
-    const location = document.getElementById('locationInput').value.trim().toUpperCase();
+    const location = document.getElementById('locationInput').value.trim();
 
       setTimeout(() => {
         if (location === "") {
-            reject("Please enter a currency");
+            reject("Please enter a location");
           } else {
         // console.log(`User entered location of ${location}`);
         resolve(location);
           }
-      }, 1000);
+      }, 500);
     });
   }
+
+// Async function to retrieve weather information
+
+  const retrieveWeatherInformation = async (location) => {
+    const apiKey = '3935132cd748d97f82e7ea2ee0b91c85'
+    const weatherInfoURL = `http://api.openweathermap.org/data/2.5/weather?q=${location},uk&APPID=${apiKey}&units=metric`;
+    
+    try {
+        const response = await fetch(weatherInfoURL);
+        const data = await response.json();
+
+        if (data.cod !== 200) {
+            throw new Error(data.message);
+        } else {
+            const weather = data.weather[0].description;
+            const temp = data.main.temp;
+            echoBlock.textContent = `The weather in ${location} is ${weather} with a temperature of ${temp} °C`;
+        }
+          // console.log('Rates against GBP are: ', JSON.stringify(data, null, 2));
+    } catch (err) {
+      echoBlock.textContent = `Error fetching weather information: ${err.message}. Please enter a valid location e.g. London`;
+  }
+};
+
+//All DOM elements
+const btn = document.querySelector('button');
+const input = document.querySelector('input');
+const echoBlock = document.getElementById('echo');
+
+// const echoOutput = (event) => {
+    
+//     if (input.value === '') {
+//         alert('Please enter a location');
+//     }
+//     else {
+//         echoBlock.textContent = input.value;
+//     }
+// }
+
+// btn.addEventListener('click', echoOutput);
+
+btn.addEventListener('click', async () => {
+try {
+    // Get user input 
+    const location = await locationInput();
+    // Fetch and display the weather for the location
+    await retrieveWeatherInformation(location);
+} catch (err) {
+    // Handle error
+    alert(err);
+}
+});
